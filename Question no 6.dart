3383abea41c5e226521dd470aebfb64126862214
MahadAnsari18 <mahadansari18@gmail.com>
2024-12-11 19:18:24 +0500

    Question 6

---
+++ Question no 6.dart
@@ -0,0 +1,13 @@
+// Q.6: Write a program to check whether an alphabet is a vowel or consonant.
+
+void main(){
+  String letter = "a";
+if(letter=="a" || letter=="e" || letter=="i" || letter=="o" || letter=="u"){
+  print("The letter is a vowel");
+}else{
+  print("The letter is a constant");
+}
+}
+
+// Answer is->
+//           The letter is a vowel
